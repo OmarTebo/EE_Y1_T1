8cc39aaeb8fea8836d9f1e22397a1b6872ef8044
OmarTebo <eltabarany.omar@gmail.com>
2023-11-10 19:43:07 +0200

    removed unused files and add new untracked

--- Intro to Programming/intro_to_prog_report_1.c
+++ Intro to Programming/intro_to_prog_report_1.c
@@ -4,4 +4,17 @@
 
 int main()
 {
+    float r,
+          area,
+          circumference;
 
+    printf("Enter the radius of circle> ");
+    scanf("%f", &r);
+
+    circumference = 2 * PI * r;
+    area = PI * r * r;
+
+    printf("\nFor a circle with radius of %.2lf\n", r);
+    printf("The Circumference = %.2lf\n", circumference);
+    printf("The Area = %.2lf\n", area);
+}
